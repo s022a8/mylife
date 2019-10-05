0194aede019e3b21d6d760ad5806834fe3e1849c
s022a8 <kafuka821shiro@gmail.com>
2019-10-05 16:48:45 +0900

    Add controllers except devise-controller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,7 @@
+class Admin::UsersController < ApplicationController
+  def index
+  end
+
+  def warning
+  end
+end
---
+++ spec/views/admin/users/warning.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "users/warning.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
