918137c96ccf30fe0bc1c067ccccd0b55e926570
s022a8 <kafuka821shiro@gmail.com>
2019-10-05 18:38:28 +0900

    Add routes except devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,55 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/index'
-    get 'users/warning'
+  
+  ###ルートURL###
+  root 'users#index'
+
+
+  ##Devise関連
+
+
+  ##エンドユーザ側##
+  #UsersController
+  resource :users, only: [:show, :edit, :update] do
+    collection do
+      get 'warning'
+      delete 'leave'
+    end
   end
-  get 'static_pages/about'
-  get 'static_pages/tos'
-  get 'book_marks/index'
-  get 'talks/index'
-  get 'talks/show'
-  get 'histories/index'
-  get 'histories/show'
-  get 'histories/new'
-  get 'histories/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+
+  #HistoriesController
+  resources :users, only: [] do
+    resources :histories
+  end
+
+  #TalksController
+  resources :talks, only: [:index, :show, :create] do
+  #MessagesController
+    resources :messages, only: [:create]
+  end
+
+  #CommentsController
+  resources :histories, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  #BookMarksController
+  resources :book_marks, only: [:index, :create, :destroy]
+
+  #StaticPagesController
+  get '/static_pages/about', to: 'static_pages#about'
+  get '/static_pages/tos', to: 'static_pages#tos'
+  
+
+
+  ##管理者側##
+
+  #Admin::UsersController
+  namespace :admin do
+    get '', to: 'users#index'
+    get 'users/:id/warning', to: 'users#warning'
+    delete 'users/:id/remove', to: 'users#remove'
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
