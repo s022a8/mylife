10bfc027def18bb19fb09b57e0d70a98f020c7a6
s022a8 <kafuka821shiro@gmail.com>
2019-10-08 05:07:23 +0900

    Fix browser-back-error

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -9,10 +9,25 @@ class TalksController < ApplicationController
   end
 
   def create
+    #トークを初めた直後にブラウザバックしてしまったときの対処
+    current_user_entries = current_user.entries
+    other_user_entries = User.find(params[:entry][:user_id]).entries
+    current_user_entries.each do |cue|
+      other_user_entries.each do |oue|
+        if cue.room.id == oue.room.id
+          flash[:notice] = "トークルームは既に作成されています。"
+          flash[:alert] = "ブラウザバックした場合に、このメッセージが表示されます。"
+          redirect_to talk_path(cue.room.id) and return
+        end
+      end
+    end
+
+
     @room = Room.create
     @entry1 = current_user.entries.build(room_id: @room.id, user_id: current_user.id)
     @entry2 = Entry.new(entry_params.merge(room_id: @room.id))
     if @entry1.save && @entry2.save
+      flash[:notice] = "トークルームが作成されました。"
       redirect_to talk_path(@room.id)
     else
       flash[:alert] = "トークを始めることができませんでした。"
