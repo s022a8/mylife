f96bb6519ca896fde23fbf6f9a8ca1ff4ec7b3fa
s022a8 <kafuka821shiro@gmail.com>
2019-10-08 22:51:57 +0900

    Fix header

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,16 @@
+(function() {
+    $(function() {
+        $(".header-nav-div").mouseover(function() {
+            $(this).css({
+                'backgroundColor':'rgba(114, 195, 206, 0.2)'
+            });
+        });
+
+        $(".header-nav-div").mouseout(function() {
+            $(this).css({
+                'backgroundColor':'white'
+            });
+        });
+    });
+
+})();
