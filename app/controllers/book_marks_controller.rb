daafb0a32a8a77701fc1e5fcd54a2427186914b3
s022a8 <kafuka821shiro@gmail.com>
2019-10-09 02:56:06 +0900

    Add book_mark-function

--- app/controllers/book_marks_controller.rb
+++ app/controllers/book_marks_controller.rb
@@ -1,6 +1,25 @@
 class BookMarksController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
   end
+
+  def create
+    @history = History.find(params[:history_id])
+    current_user.book_mark(@history)
+    respond_to do |format|
+      format.html { redirect_to user_histories_path(current_user.id) }
+      format.js
+    end
+  end
+
+  def destroy
+    @history = History.find(params[:id])
+    current_user.un_book_mark(@history)
+    respond_to do |format|
+      format.html { redirect_to user_histories_path(current_user.id) }
+      format.js
+    end
+  end
+
 end
--- app/models/book_mark.rb
+++ app/models/book_mark.rb
@@ -1,4 +1,6 @@
 class BookMark < ApplicationRecord
     belongs_to :user
     belongs_to :history
+
+    validates_uniqueness_of :user_id, scope: :history_id
 end
