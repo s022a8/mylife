c252ce0117baa42f81b0ec9cb91b9924420ebcad
s022a8 <kafuka821shiro@gmail.com>
2019-10-13 02:35:25 +0900

    Add omniauth-login-facebook only local (this inplement is not perfect)

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,22 +37,41 @@ class User < ApplicationRecord
   end
 
 
-  ##ブックマーク機能について
-  #既にブックマークしているか？
+  ## ブックマーク機能について
+  # 既にブックマークしているか？
   def book_mark?(history)
     book_marks.exists?(history_id: history.id)
   end
 
-  #ブックマークする
+  # ブックマークする
   def book_mark(history)
     book_mark = book_marks.build(history_id: history.id)
     book_mark.save
   end
 
-  #ブックマークをやめる
+  # ブックマークをやめる
   def un_book_mark(history)
     book_mark = book_marks.find_by(history_id: history.id)
     book_mark.destroy
+  end
+
+
+  ## omniauth認証
+  # findメソッド
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        name: auth.info.name,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
   end
 
 end
