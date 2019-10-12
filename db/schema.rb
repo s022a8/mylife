c7a97aa06466db60ea3c8a8e9c13785b28ac0be2
s022a8 <kafuka821shiro@gmail.com>
2019-10-12 23:38:44 +0900

    Setup facebook-authentification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_102951) do
+ActiveRecord::Schema.define(version: 2019_10_12_142730) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -131,6 +131,8 @@ ActiveRecord::Schema.define(version: 2019_10_07_102951) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
