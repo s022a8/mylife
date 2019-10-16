37e22d2f87759b173f9ac6103aa7c5c1f394d1bd
s022a8 <kafuka821shiro@gmail.com>
2019-10-16 14:31:39 +0900

    Make questionnaire-index improve

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -5,5 +5,5 @@ class Questionnaire < ApplicationRecord
     accepts_nested_attributes_for :questionnaire_items, 
                     reject_if: proc { |attributes| attributes['content'].blank? }, allow_destroy: true
 
-    validates :theme, presence: true, length: { maximum: 100, minimum: 2 }
+    validates :theme, presence: true, length: { maximum: 132, minimum: 2 }
 end
